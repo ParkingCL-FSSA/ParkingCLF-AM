f93cf68526735291f546f6cf3fb8bfc44ad9aca1
Parcheggio CL Fontanarossa <parkingclf.am@gmail.com>
2026-05-11 01:03:19 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -149,7 +149,7 @@ async function doLogin() {
         
             try {
         
-                const resDisp = await fetch(`/api/disponibilita/${npass}`);
+                const resDisp = await fetch(`/api/disponibilita/${userPass}`);
                 disponibilitaGiorni = await resDisp.json();
                 
                 buildCal(); 
@@ -181,7 +181,7 @@ async function aggiornaPostiLiberiPiantone() {
 
 async function caricaDisponibilita() {
 
-    const res = await fetch(`/api/disponibilita/${npass}`);
+    const res = await fetch(`/api/disponibilita/${userPass}`);
 
     if (!res.ok) {
         console.error("Errore disponibilità");
