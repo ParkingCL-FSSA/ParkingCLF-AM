9e2e12a49ea69adae8e5c6509cb215c301797160
Parcheggio CL Fontanarossa <parkingclf.am@gmail.com>
2026-05-11 02:53:16 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -236,10 +236,9 @@ async function inviaPren() {
         document.getElementById('summary-details').innerHTML =
             `<b>Pass:</b> ${userPass}<br><b>Dal:</b> ${fmtData(selectedDays[0])}<br><b>Al:</b> ${fmtData(selectedDays[selectedDays.length - 1])}`;
         show('view-success');
-        mostraMie();
-       // setTimeout(() => {
-        //    mostraMie();
-      //  }, 10000);
+         setTimeout(() => {
+         mostraMie();
+         }, 5000);
     } else {
         // Gestisci errori di validazione dal server
         const err = await res.json();
