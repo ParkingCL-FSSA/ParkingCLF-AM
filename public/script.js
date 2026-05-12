29a250a3c6217eeb77e2f85d56ad65b0580f467c
Parcheggio CL Fontanarossa <parkingclf.am@gmail.com>
2026-05-12 15:45:59 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -212,7 +212,10 @@ async function inviaPren() {
         alert('Inserisci la tua mail privata');
         return;
     }
-    if (!selectedDays.length || !email) return alert("Dati mancanti!");
+    if (!selectedDays.length || !email) return alert("inserisci la tua email!");
+    if (selectedDays.length < 2) {
+        return alert("Per il parcheggio <Lunga Sosta> il minimo di giorni prenotabili sono 2");
+    }
     if (selectedDays.length > 15) return alert("Massimo 15 giorni selezionabili!");
 
    const res = await fetch('/api/prenota', {
