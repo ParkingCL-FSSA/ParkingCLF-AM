e04af29fad0537f8adf7eb6024757dc755c45da1
Parcheggio CL Fontanarossa <parkingclf.am@gmail.com>
2026-05-12 17:29:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,14 +39,7 @@ function clean(input) {
         .toUpperCase()
         .substring(0, 5);
 }
-function resetSelezione() {
 
-    selectedDays = [];
-
-    document
-        .querySelectorAll('.day-slot.selected')
-        .forEach(el => el.classList.remove('selected'));
-}
 // ⏰ JOB SCADENZA
 async function scadenzaPrenotazioni() {
     try {
