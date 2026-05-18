ebf6e3534bc120fa90749fc38694e3a583cc1985
Parcheggio CL Fontanarossa <parkingclf.am@gmail.com>
2026-05-18 20:13:44 +0200

    Reorganize login success message handling

--- public/script.js
+++ public/script.js
@@ -256,11 +256,7 @@ async function doLogin() {
                 .value
                 .trim()
                 .toUpperCase();
-        if (data.valid) {
-        // Nascondi l'avviso quando il login ha successo
-        const avviso = document.getElementById('avviso-manutenzione');
-        if (avviso) avviso.style.display = 'none';
-        }
+        
             if (!userPass) return;
     
             const res = await fetch('/api/valida-pass', {
@@ -281,7 +277,11 @@ async function doLogin() {
                 alert("Accesso Negato");
                 return;
             }
-       
+       if (data.valid) {
+        // Nascondi l'avviso quando il login ha successo
+        const avviso = document.getElementById('avviso-manutenzione');
+        if (avviso) avviso.style.display = 'none';
+        }
             if (data.ruolo === 'piantone') {
     
                 show('view-piantone');
